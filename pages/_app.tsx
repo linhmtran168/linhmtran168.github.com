57684348e98860d86c9246f2236e952108799f90
Linh M. Tran <linhtm@vietnamlab.vn>
2021-01-25 14:39:53 +0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import { pageview } from '../libs/gtag';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+  const handleRouteChange = (url: string): void => {
+    pageview(url);
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  });
+
   return <Component {...pageProps} />;
 }
 
